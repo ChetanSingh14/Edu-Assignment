328a05e0700ad96eb0a2acd9346ad6b12cff8e3d
ChetanSingh14 <siradhanachetan14@gmail.com>
2025-03-07 23:31:03 +0530

    updated

--- school-management-api/config/db.js
+++ school-management-api/config/db.js
@@ -3,7 +3,7 @@ const mysql = require('mysql2');
 const db = mysql.createConnection({
     host: "localhost",
     user: "root",
-    password: "Gurjar_14",
+    password: "Prateek@2109",
     database: "school_management"
 });
 
